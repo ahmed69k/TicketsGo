fba8479c53855761597bdeb9f840ceceed76785a
ahmed69k <ahmedassem04@gmail.com>
2025-03-03 21:52:31 +0200

    Updated

--- Models/CRUD/CRUD.js
+++
@@ -1,5 +0,0 @@
-const UserModel = require("./Models/UserSchema.js");
-let User = new StudentModel({
-    name : "Ahmed"
-}) 
-StudentModel.find({name: "Ahmed"})
--- Models/CRUD/Connection.js
+++
@@ -1,4 +0,0 @@
-const {MongoClient}= require("mongodb")
-const url = "mongodb://localhost:27017/"
-const mongoose = require('mongoose')
-MongoClient.connect("mongodb://localhost:27017/", (err,Client)=>{})
