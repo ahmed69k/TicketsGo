c5542c218afd311f69240f8640488b94719c914d
ahmed69k <ahmedassem04@gmail.com>
2025-04-11 17:10:40 +0200

    ..

--- Middleware/AuthenticationMiddleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  // Example authentication logic
-  const token = req.headers.authorization;
-  if (!token) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  // Validate token logic here...
-  next();
-};
