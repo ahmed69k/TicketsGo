a866e5c480163fb210a5385ea9f8e8339a31b622
ahmed69k <ahmedassem04@gmail.com>
2025-05-24 00:29:35 +0300

    edit event done

--- frontend/src/pages/EditEvent.jsx
+++ frontend/src/pages/EditEvent.jsx
@@ -22,7 +22,24 @@ function EditEvent() {
     const fetchEvent = async () => {
       try {
         const res = await api.get(`/events/${id}`);
+        const event = res.data
+        if(event.date){
+          const d = new Date(event.date);
+        const pad = (n) => n.toString().padStart(2, '0');
+        const formatted =
+          d.getFullYear() +
+          '-' +
+          pad(d.getMonth() + 1) +
+          '-' +
+          pad(d.getDate()) +
+          'T' +
+          pad(d.getHours()) +
+          ':' +
+          pad(d.getMinutes());
+        event.date = formatted;
+        }
         setFormData(res.data);
+        
       } catch (err) {
         console.error("Failed to fetch event:", err);
       }
