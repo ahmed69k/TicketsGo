807c2cb0c1591b743c67009fe1853521b670a99a
ahmed69k <ahmedassem04@gmail.com>
2025-05-24 04:39:40 +0300

    styling lowk

--- frontend/src/pages/UpdateUserRolePage.jsx
+++ frontend/src/pages/UpdateUserRolePage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../services/api";
 import { toast } from "react-toastify";
+import '../styling/UpdateRole.css'
 
 function UpdateUserRolePage() {
   const { id } = useParams();
